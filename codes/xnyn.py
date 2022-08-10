71b46d226f8a0b278b689e31df2f9831697570bf
Sparsh Gupta <83421688+Sparsh-gupta26@users.noreply.github.com>
2022-08-10 11:17:45 +0530

    Add files via upload

--- codes/xnyn.py
+++ codes/xnyn.py
@@ -1,28 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
-#If using termux
-import subprocess
-import shlex
-#end if
-
-
 
 x=np.array([1.0,2.0,3.0,4.0,2.0,1.0])
 k = 20
-y = np.zeros(20)
+# y = np.zeros(20)
 
+y = np.loadtxt('program.dat', dtype = "double")
 
-y[0] = x[0]
-y[1] = -0.5*y[0]+x[1]
+# y[0] = x[0]
+# y[1] = -0.5*y[0]+x[1]
 
-for n in range(2,k-1):
-	if n < 6:
-		y[n] = -0.5*y[n-1]+x[n]+x[n-2]
-	elif n > 5 and n < 8:
-		y[n] = -0.5*y[n-1]+x[n-2]
-	else:
-		y[n] = -0.5*y[n-1]
-print(y)
+# for n in range(2,k-1):
+# 	if n < 6:
+# 		y[n] = -0.5*y[n-1]+x[n]+x[n-2]
+# 	elif n > 5 and n < 8:
+# 		y[n] = -0.5*y[n-1]+x[n-2]
+# 	else:
+# 		y[n] = -0.5*y[n-1]
+# print(y)
 
 #subplots
 plt.subplot(2, 1, 1)

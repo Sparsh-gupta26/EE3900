93bd1a0aa7788442a518d99fd55976114982bbdf
Sparsh Gupta <83421688+Sparsh-gupta26@users.noreply.github.com>
2022-08-16 23:58:42 +0530

    Add files via upload

--- codes/dtft.py
+++ codes/dtft.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-#if using termux
-import subprocess
-import shlex
-#end if
+# #if using termux
+# import subprocess
+# import shlex
+# #end if
 
 
 #DTFT
@@ -16,7 +16,12 @@ def H(z):
 
 
 #Input and Output
-omega = np.linspace(-3*np.pi,3*np.pi,1e2)
+omega = np.linspace(-3*np.pi,3*np.pi,100)
+for i in omega:
+	#print(abs(H(np.exp(1j*i))), " ", abs(H(np.exp(0))))
+	if abs(abs(H(np.exp(0))) -  abs(H(np.exp(1j*i)))) <= 0.01:
+		print("Period of Distcrete Time Fourier Transform is", abs(i))
+		break
 
 #subplots
 plt.plot(omega, abs(H(np.exp(1j*omega))))
@@ -24,13 +29,14 @@ plt.title('Filter Frequency Response')
 plt.xlabel('$\omega$')
 plt.ylabel('$|H(e^{\jmath\omega})| $')
 plt.grid()# minor
+plt.show()
 
-#if using termux
-plt.savefig('../figs/dtft.pdf')
-plt.savefig('../figs/dtft.eps')
-subprocess.run(shlex.split("termux-open ../figs/dtft.pdf"))
-#else
-#plt.show()
+# #if using termux
+# plt.savefig('../figs/dtft.pdf')
+# plt.savefig('../figs/dtft.eps')
+# subprocess.run(shlex.split("termux-open ../figs/dtft.pdf"))
+# #else
+# #plt.show()
 
 
 
